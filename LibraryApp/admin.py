d181281bd6f9681a996821c894cf3197401e3a0b
Forlon <m.fabianowski99@gmail.com>
2020-12-14 19:15:20 +0100

    Rejestracja modeli

--- LibraryApp/admin.py
+++ LibraryApp/admin.py
@@ -11,13 +11,50 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('book_id', 'title', 'category')
 
 
-admin.site.register(Book)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = ('borrowing_id', 'start_date', 'end_date',
+                    'isbn', 'user')
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('card_id', 'number', 'token')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'name', 'description')
+
+
+class EditionAdmin(admin.ModelAdmin):
+    list_display = ('isbn', 'number', 'page_count', 'date', 'book',
+                    'publisher')
+
+
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('payments_id', 'amount', 'date', 'user')
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('publisher_id', 'name')
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('subscription_id', 'name', 'price',
+                    'borrowing_count')
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'name', 'email', 'hash',
+                    'borrowings_remaining', 'subscription',
+                    'card')
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Category)
-admin.site.register(Edition)
-admin.site.register(Publisher)
-admin.site.register(Borrowing)
-admin.site.register(User)
-admin.site.register(Card)
-admin.site.register(Subscription)
-admin.site.register(Payments)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Edition, EditionAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Borrowing, BorrowingAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(Payments, PaymentsAdmin)
