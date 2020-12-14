f51487464826a7d42cf076885559abfbd27b5cab
Nyatan <domino.ndg@o2.pl>
2020-12-14 19:17:07 +0100

    Basic views and templates

--- LibraryApp/views.py
+++ LibraryApp/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'polls/index.html')
+
+
+def login(request):
+    return render(request, 'polls/login.html')
+
+
+def browse(request):
+    return render(request, 'polls/browse.html')
+
+
+def register(request):
+    return render(request, 'polls/register.html')
+
+
+def read(request):
+    return render(request, 'polls/read.html')
+
+
+def user(request):
+    return render(request, 'polls/user.html')
